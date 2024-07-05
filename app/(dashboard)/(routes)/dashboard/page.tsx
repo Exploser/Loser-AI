6ccab3e54771083e9bae71bf6059198680f07dc3
Exploser <exploserbusiness@gmail.com>
2024-07-05 11:18:45 -0400

    Add basic structure for the project

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <Button variant="default" size="lg">
+      Click me!
+    </Button>
+  );
+}
+
+export default DashboardPage;
