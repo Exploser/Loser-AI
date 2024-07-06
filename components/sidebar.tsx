2330a7a186c990b664bab21c1b90f98d72df8d39
Exploser <exploserbusiness@gmail.com>
2024-07-06 13:35:36 -0400

    Update ui to highlight current path

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,6 +5,7 @@ import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, Video
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({
     weight: "600",
@@ -56,6 +57,7 @@ const routes = [
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname();
     return ( 
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -69,7 +71,7 @@ const Sidebar = () => {
                 </Link>
                 <div className="space-y-1">
                     {routes.map((route, index) => (
-                        <Link key={index} href={route.href} className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+                            <Link key={index} href={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname===route.href ? "text-white bg-white/10" : 'text-zinc-400')}>
                         <div className="flex items-center flex-1">
                             <route.icon className={cn("w-6 h-6 mr-4", route.color)}/>
                             {route.label}
