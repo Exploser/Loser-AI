6063be73dbea415074446047bf5112df72dfa2a3
Exploser <exploserbusiness@gmail.com>
2024-07-06 14:15:03 -0400

    Fix typo on sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -20,9 +20,9 @@ const routes = [
         color: 'text-sky-500',
     },
     {
-        label: "Conversion",
+        label: "Conversation",
         icon: MessageSquare,
-        href: "/conversion",
+        href: "/conversation",
         color: 'text-violet-500',
     },
     {
