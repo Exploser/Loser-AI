fb60af31cc0ad3804377577b034ad8578fb1b4f6
Exploser <exploserbusiness@gmail.com>
2024-07-09 13:16:48 -0400

    Add free usage counter in the sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,6 +6,7 @@ import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FreeCounter } from "./free-counter";
 
 const montserrat = Montserrat({
     weight: "600",
@@ -56,10 +57,14 @@ const routes = [
     },
 ]
 
-const Sidebar = () => {
+interface SidebarProps {
+    apiLimitCount: number;
+}
+
+const Sidebar = ({apiLimitCount = 0}: SidebarProps) => {
     const pathname = usePathname();
     return ( 
-        <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+        <div className="space-y-4 py-4 flex flex-col h-[100vh] bg-[#111827] text-white justify-between ">
             <div className="px-3 py-2 flex-1">
                 <Link href={"/dashboard"} className="flex items-center pl-3 mb-14">
                     <div className="relative w-8 h-8 mr-4">
@@ -80,6 +85,7 @@ const Sidebar = () => {
                     ))}
                 </div>
             </div>
+            <FreeCounter apiLimitCount={apiLimitCount}/>
         </div>
      );
 }
