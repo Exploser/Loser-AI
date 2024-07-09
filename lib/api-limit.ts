c7f9718862abf1a8fcc6c2d5201098b1b719d7da
Exploser <exploserbusiness@gmail.com>
2024-07-09 12:14:45 -0400

    Add db functions to check for free usage

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -1,17 +1,64 @@
 import { auth } from "@clerk/nextjs/server";
 import { userApiLimit } from "../server/drizzle/schema";
-import { and, eq } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { db } from "../server/drizzle/index";
-
+import { MAX_FREE_COUNTS } from "../constants";
 
 export const increaseApiLimit = async () => {
     const { userId } = auth();
 
-    if (!userId) throw new Error("Unauthorized");
-    
+    if (!userId) {
+        throw new Error("Unauthorized");
+    }
 
     const userApiUsage = await db.query.userApiLimit.findMany({
-        where: (model, { eq }) => eq(model.userId, userId),
+        where: (model) => eq(model.userId, userId),
     });
-    return userApiUsage;
+
+    if (userApiUsage.length > 0) {
+        await db.update(userApiLimit)
+            .set({ count: userApiUsage[0].count + 1 })
+            .where(eq(userApiLimit.userId, userId))
+            .execute();
+    } else {
+        await db.insert(userApiLimit)
+            .values({ userId, count: 1 })
+            .execute();
+    }
+};
+
+export const checkApiLimit = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return false;
+    }
+
+    const userApiUsage = await db.query.userApiLimit.findMany({
+        where: (model) => eq(model.userId, userId),
+    });
+
+    if (userApiUsage.length === 0 || userApiUsage[0].count < MAX_FREE_COUNTS) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const getApiLimitCount = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return 0;
+    }
+
+    const userApiUsage = await db.query.userApiLimit.findMany({
+        where: (model) => eq(model.userId, userId),
+    });
+
+    if (userApiUsage.length === 0) {
+        return 0;
+    }
+
+    return userApiUsage[0].count;
 };
