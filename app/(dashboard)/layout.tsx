dd005241fb4b67a98debfbba3aa20ffa53197d7b
Exploser <exploserbusiness@gmail.com>
2024-07-09 13:15:24 -0400

    Fetch api usage count on dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,15 +1,19 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
 import { ReactNode } from "react";
 
-const DashboardLayout = ({
+const DashboardLayout = async ({
     children
 }: {children :ReactNode}) => {
+
+    const apiLimitCount = await getApiLimitCount();
+
     return (  
         <div className="dashboard-layout__content h-full relative">
-            <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-slate-900 md:w-72">
+            <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[50] bg-slate-900 md:w-72">
                 <div>
-                    <Sidebar />
+                    <Sidebar apiLimitCount={apiLimitCount} />
                 </div>
             </div>
             <main className="md:pl-72">
