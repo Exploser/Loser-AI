cf0fc5715860d12bdec442f0cd022f6bd7cbed11
Exploser <exploserbusiness@gmail.com>
2024-07-11 10:19:25 -0400

    Update background color

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,7 +12,7 @@ const DashboardLayout = async ({
     const isPro = await checkSubscription();
 
     return (  
-        <div className="dashboard-layout__content h-full relative">
+        <div className="dashboard-layout__content h-full relative bg-[#ffffff]">
             <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[50] bg-slate-900 md:w-72">
                 <div>
                     <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
